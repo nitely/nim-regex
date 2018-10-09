af77dc94f0d9adc4055ae53bb2f8850f0abee863
Timothee Cour <timothee.cour2@gmail.com>
2018-10-08 17:38:22 -0700

    add inline

--- src/regex.nim
+++ src/regex.nim
@@ -2850,15 +2850,15 @@ proc toPattern*(s: string): Regex {.raises: [RegexError].} =
     namedGroups: names)
 
 when defined(forceRegexAtRuntime):
-  proc re*(s: string): Regex =
+  proc re*(s: string): Regex {.inline.} =
     toPattern(s)
 else:
-  proc re*(s: static string): Regex =
+  proc re*(s: static string): Regex {.inline.} =
     ## Parse and compile a regular expression at compile-time
     const pattern = toPattern(s)
     pattern
 
-  proc re*(s: string): Regex =
+  proc re*(s: string): Regex {.inline.} =
     ## Parse and compile a regular expression at run-time
     toPattern(s)
 

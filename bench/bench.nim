a8064d70c534d0be11cb205a9226c73bb68c48c2
nitely <ecastroborsani@gmail.com>
2023-09-30 05:00:34 -0300

    bench

--- bench/bench.nim
+++ bench/bench.nim
@@ -163,6 +163,26 @@ benchRelative(regex_ip_find_all, m):
   doAssert d == 5
   doNotOptimizeAway(d)
 
+var sql_find_all_re = re.re"mysql://[^/\s?#]+[^\s?#]+(?:\?[^\s#]*)?(?:#[^\s]*)?"
+
+bench(re_sql_find_all, m):
+  var d = 0
+  for i in 0 ..< m:
+    for _ in re.findAll(bench_text, sql_find_all_re):
+      d += 1
+  doAssert d == 4
+  doNotOptimizeAway(d)
+
+const sql_find_all = regex.re2"mysql://[^/\s?#]+[^\s?#]+(?:\?[^\s#]*)?(?:#[^\s]*)?"
+
+benchRelative(regex_sql_find_all, m):
+  var d = 0
+  for i in 0 ..< m:
+    for _ in regex.findAll(bench_text, sql_find_all):
+      d += 1
+  doAssert d == 4
+  doNotOptimizeAway(d)
+
 var url_find_all_re = re.re"https?://[^/\s?#]+[^\s?#]+(?:\?[^\s#]*)?(?:#[^\s]*)?"
 
 bench(re_url_find_all, m):

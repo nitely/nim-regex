5d5b167107d17dd3ee8accd3e0af01b932ce3746
nitely <ecastroborsani@gmail.com>
2025-01-18 03:01:45 -0300

    ugh

--- src/regex.nim
+++ src/regex.nim
@@ -508,16 +508,28 @@ proc reCheck(s: string) {.compileTime.} =
   except RegexError:
     raise newException(RegexError, getCurrentExceptionMsg())
 
-func `~`*(s: static string): Regex2 =
+import locks
+var sigils {.compileTime.} = newSeq[string]()
+var sigilre = initTable[string, Regex2]()
+var sigillock: Lock
+initLock(sigillock)
+
+func `~`*(s: static string): Regex2 {.raises: [RegexError].} =
   ## Compile a regex at runtime.
   ## The compiled regex is cached and reused.
   ## It gets compiled once in a program lifetime.
   ## The regex is validated at compile-time,
   ## and so it may only raise a `RegexError` at compile-time.
   static: reCheck(s)
-  {.cast(noSideEffect), cast(gcsafe).}:
-    var reg {.global.} = toRegex2 reImpl(s)
-    return reg
+  when nimvm:
+    return toRegex2 reImpl(s)
+  else:
+    {.cast(noSideEffect), cast(gcsafe), cast(raises: [RegexError]).}:
+      withLock sigillock:
+        if s in sigilre:
+          return sigilre[s]
+        sigilre[s] = toRegex2 reImpl(s)
+        return sigilre[s]
 
 func group*(m: RegexMatch2, i: int): Slice[int] {.inline, raises: [].} =
   ## return slice for a given group.
